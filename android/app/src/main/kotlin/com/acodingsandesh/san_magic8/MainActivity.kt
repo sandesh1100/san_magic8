cd6f4ea48f154b0bc49506b0ea89307b18f5921c
Sandesh <sandesh.bais@mobicule.com>
2024-07-22 22:50:10 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/acodingsandesh/san_magic8/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.acodingsandesh.san_magic8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: BallPage(),
+  ));
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Ask Me Anything",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Center(child: Ball()),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset("images/ball$ballNumber.png"));
+  }
+}
